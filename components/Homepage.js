55d8530eaf61d78e404fde4224c9c0b0b80f0f03
unknown <niwanoleiko@gmail.com>
2023-03-05 01:23:25 -0500

    Finished Homepage

--- components/Homepage.js
+++ components/Homepage.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Button } from "native-base";
 
 // getting a weird ts error here, import when fixed
-// import tremeloo-logo from '../assets/tremeloo-logo.png';
 
 const Homepage = () => {
+
     return (
         // add my logo to the top of the page around here
         <View style={styles.container}>
-            <Text style={styles.text}>Connecting you to the music you want to hear</Text>
+            <Image
+                source={require('../assets/logo.png')}
+                style={styles.logo}
+                // put this at the top of the page
+
+            />
+            <Text style={styles.text}>Tremeloo</Text>
+            <Text style={styles.text}>Connecting you to the music you want to listen to</Text>
+            <Button onPress={() => console.log("Login to Spotify")}>Login to Spotify</Button>
         </View>
     );
 }
@@ -22,7 +31,12 @@ const styles = StyleSheet.create({
     },
     text: {
         color: '#a3e0dc',
-    }
+    },
+    logo: {
+        width: 200,
+        height: 200,
+        marginBottom: 20
+    },
 });
 
 
