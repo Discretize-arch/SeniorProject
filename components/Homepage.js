0edf528e1728758554042e24add7c4663f366c8a
unknown <niwanoleiko@gmail.com>
2023-03-07 10:45:03 -0500

    Added framework to SongRecs and YourPlaylist. native-base is wonky rn so commented out.

--- components/Homepage.js
+++ components/Homepage.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { Button } from "native-base";
-
-// getting a weird ts error here, import when fixed
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button } from 'react-native';
+// import { NativeBaseProvider, Button } from "native-base";
 
 const Homepage = () => {
 
     return (
         // add my logo to the top of the page around here
-        <View style={styles.container}>
-            <Image
-                source={require('../assets/logo.png')}
-                style={styles.logo}
-                // put this at the top of the page
-
-            />
-            <Text style={styles.text}>Tremeloo</Text>
-            <Text style={styles.text}>Connecting you to the music you want to listen to</Text>
-            <Button onPress={() => console.log("Login to Spotify")}>Login to Spotify</Button>
-        </View>
+            <View style={styles.container}>
+                <Image source={require('../assets/logo.png')} style={styles.logo}/>
+                <Text style={styles.text}>Tremeloo</Text>
+                <Text style={styles.text}>Connecting you to the music you want to listen to</Text>
+                <Button
+                    title="Login to Spotify"
+                    onPress={() => Alert.alert('Button pressed')}
+                />
+                <Text style={styles.text}> Connect to your Spotify button</Text>
+            </View>
     );
 }
 
