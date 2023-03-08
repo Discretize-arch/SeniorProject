06092b1fc640f61b785c6d5e44198b7f4070ceef
Vincent Green II <vgreen22005@icloud.com>
2023-03-08 12:52:06 -0500

    recommendation container added

--- components/SongRecs.js
+++ components/SongRecs.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
-const SongRecs = () => {
+const SongRecs = ({ navigation }) => {
 
     return (
         <View style={styles.container}>
+
             <View style={styles.bannerContainer}>
-                <Image source={require('../assets/banner.png')} style={styles.logo} />
+                <Image source={require('../assets/banner.png')} style={styles.banner} />
             </View>
 
+            <View style={styles.playlistHeader}>
+                <Text style={styles.headerText}>Recommended Songs</Text>
+            </View>
 
-            <Text style={styles.text}>Tremeloo</Text>
-            <Text style={styles.text}>Connecting you to the music you want to listen to</Text>
-            <Text style={styles.text}> Connect to your Spotify button</Text>
+            <View style={styles.playlistContainer}>
+                <Text style={styles.text}>Song list goes here</Text>
+                
+                <View style={styles.playlistElement}>
+                    <Text style ={styles.playlistImage}>
+                        artist image
+                    </Text>
+                    
+                    
+                    <Text style ={styles.songName}>
+                        Song Name Here
+                    </Text>
+                
+                    
+                    <Button style = {styles.songButton}
+                        title='Add to Playlist'
+                        onPress={() => navigation.navigate('Song Recommendations')}
+                    />
+
+                </View>
+
+            </View>
+
         </View>
     );
 }
@@ -29,10 +53,48 @@ const styles = StyleSheet.create({
     },
     bannerContainer: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
     },
-});
+    banner: {
+        flexShrink: 1,
+        flexGrow: 1,
+    },
 
+    playlistHeader: {
+        flex: 1 / 8,
+        justifyContent: 'flex-start',
+    },
+    headerText: {
+        color: '#a3e0dc',
+        fontSize: 26,
+    },
+
+    playlistContainer: {
+        flex: 1,
+        width: '100%',
+        //borderRadius: 20,
+        borderTopWidth: 1,
+        borderColor: '#a3e0dc',
+    },
+
+    playlistElement: {
+        flexDirection: 'row',
+    },
+    playlistImage:{
+        flex:1/2,
+        color:'#fff',
+        justifyContent: 'center',
+        textAlignVertical: 'center',
+    },
+
+    playlistButton:{
+        flex:1,
+    }, 
+    songName:{
+        flex: 1/2,
+        textAlignVertical: 'center',
+        color: '#fff',
+
+    }
+});
 
 export default SongRecs;
