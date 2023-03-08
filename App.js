e26e5cf650b2f7291db18a851da4f9a5942b49a1
Vincent Green II <vgreen22005@icloud.com>
2023-03-08 10:34:09 -0500

    added navigation functionality

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Homepage from './components/Homepage.js';
+import YourPlaylists from './components/YourPlaylists.js';
+import SongRecs from './components/SongRecs.js';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Homepage />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Your Playlists'>
+        <Stack.Screen
+          name="Home"
+          component={Homepage}
+        />
+        <Stack.Screen
+          name="Your Playlists"
+          component={YourPlaylists}
+          options={{title: 'Playlists'}}
+          
+        />
+        <Stack.Screen
+          name="Song Recommendations"
+          component={SongRecs}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
