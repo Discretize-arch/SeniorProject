8e9ab7062a33971492ee86fa68c5204f24ce1b85
Vincent Green II <vgreen22005@icloud.com>
2023-03-08 12:52:31 -0500

    playlist container added

--- components/YourPlaylists.js
+++ components/YourPlaylists.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
-const YourPlaylists = () => {
+const YourPlaylists = ({ navigation }) => {
 
     return (
-            <View style={styles.container}>
-                
-                <View style={styles.bannerContainer}>
-                    <Image source={require('../assets/banner.png')} style={styles.banner}/>
-                </View>
-                
-                <View style ={styles.playlistHeader}>
-                    <Text style={styles.text}>Your Playlists</Text>
-                </View>
-                
-                <View style={styles.playlistContainer}>
-                    <Text style={styles.text}>Connecting you to the music you want to listen to</Text>
-                    <Text style={styles.text}> Connect to your Spotify button</Text>
-                </View>
-                
+        <View style={styles.container}>
+
+            <View style={styles.bannerContainer}>
+                <Image source={require('../assets/banner.png')} style={styles.banner} />
             </View>
+
+            <View style={styles.playlistHeader}>
+                <Text style={styles.headerText}>Your Playlists</Text>
+            </View>
+
+            <View style={styles.playlistContainer}>
+                <Text style={styles.text}>Playlists go here</Text>
+                
+                <View style={styles.playlistElement}>
+                    <Text style ={styles.playlistImage}>
+                        playlist image
+                    </Text>
+
+                    <Button style = {styles.playlistButton}
+                        title='playlist Name'
+                        onPress={() => navigation.navigate('Song Recommendations')}
+                    />
+
+                </View>
+
+            </View>
+
+        </View>
     );
 }
 
@@ -39,17 +51,38 @@ const styles = StyleSheet.create({
     banner: {
         flexShrink: 1,
         flexGrow: 1,
-        //height: 280,
-        //width: 410,
     },
-    playlistContainer: {
-        
-    },
+
     playlistHeader: {
-        flex: 1, 
+        flex: 1 / 8,
         justifyContent: 'flex-start',
+    },
+    headerText: {
+        color: '#a3e0dc',
+        fontSize: 26,
+    },
+
+    playlistContainer: {
+        flex: 1,
+        width: '100%',
+        //borderRadius: 20,
+        borderTopWidth: 1,
+        borderColor: '#a3e0dc',
+    },
+
+    playlistElement: {
+        flexDirection: 'row',
+    },
+    playlistImage:{
+        flex:1/2,
+        color:'#fff',
+        justifyContent: 'center',
+        textAlignVertical: 'center',
+    },
+
+    playlistButton:{
+        flex:1,
     }
 });
-
 
 export default YourPlaylists;
